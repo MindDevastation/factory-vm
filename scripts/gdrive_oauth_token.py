142c1439271ec244e6c6183f65c6de2ce66ef5b1
Aleksey Pastukhov <mindflux99@gmail.com>
2026-02-25 19:47:54 +0200

    Add GDrive OAuth token script

---
+++ scripts/gdrive_oauth_token.py
@@ -0,0 +1,12 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+import json
+
+SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+
+flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
+creds = flow.run_local_server(port=0)
+
+with open("token.json", "w", encoding="utf-8") as f:
+    f.write(creds.to_json())
+
+print("Saved token.json")
