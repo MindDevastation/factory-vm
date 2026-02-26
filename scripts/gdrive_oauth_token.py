7dc1b7f1d6a163eb6aefbf2e1c429f16ba56a473
Aleksey Pastukhov <mindflux99@gmail.com>
2026-02-26 23:01:17 +0200

    Fixed OAuth Tokens

--- scripts/gdrive_oauth_token.py
+++ scripts/gdrive_oauth_token.py
@@ -3,7 +3,7 @@ import json
 
 SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
 
-flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
+flow = InstalledAppFlow.from_client_secrets_file("../secure/client_secret.json", SCOPES)
 creds = flow.run_local_server(port=0)
 
 with open("token.json", "w", encoding="utf-8") as f:
